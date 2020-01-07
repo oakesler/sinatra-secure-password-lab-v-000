cca3f10a9b589fd10e4277736d55b48913ef3958
Olivia Kesler <oakesler@protonmail.ch>
2020-01-07 16:14:50 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,12 +18,11 @@ class ApplicationController < Sinatra::Base
 
   post "/signup" do
     user = User.new(:username => params[:username], :password => params[:password])
-    
+    binding.pry
 		 if user.save
 		   redirect "/login"
 		  else
 		    redirect "/failure"
-		    binding.pry
 		end
   end
 
